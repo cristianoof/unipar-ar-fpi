ff57a8721f7ecf5473860fc2d70a5711df6b501c
Cristiano Fernandes <cristiano.fernandes.of@gmail.com>
2021-05-21 14:15:54 -0300

    Feito o JS para adicionar campos no formulário

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
-function clonar(){
-    var elementoOriginal = document.querySelector("#teste");
+function clonar(elemento, clonarEm){
+    var elementoOriginal = document.querySelector(elemento);
     var elementoClone = elementoOriginal.cloneNode(true);
     
-    // inserindo o elemento na paǵina
-    document.body.appendChild(elementoClone);
-}
+    // Limpa os inputs após clonar
+    if(elemento == '#mais-cursos'){
+        elementoClone.value = ""
+    }
 
-function clone2(){
-    $( ".cursos" ).clone().appendTo( ".mais-cursos" );
-}
+    if(elemento == '#mais-formacao'){
+        const limpa = elementoClone.querySelector('[data-form-input="1"]')
+        const limpa2 = elementoClone.querySelector('[data-form-input="2"]')
+        const limpa3 = elementoClone.querySelector('[data-form-input="3"]')
 
-function clone3(){
-    var $teste = $('.mais-cursos').clone();
+        limpa.value = ""
+        limpa2.value = ""
+        limpa3.value = ""
+    }
 
-    $('.cursos').appendTo($teste);
+    if(elemento == '#mais-experiencia'){
+        const limpa = elementoClone.querySelector('[data-form-input="1"]')
+        const limpa2 = elementoClone.querySelector('[data-form-input="2"]')
+        const limpa3 = elementoClone.querySelector('[data-form-input="3"]')
+        const limpa4 = elementoClone.querySelector('[data-form-input="4"]')
+        const limpa5 = elementoClone.querySelector('[data-form-input="5"]')
+
+        limpa.value = ""
+        limpa2.value = ""
+        limpa3.value = ""
+        limpa4.value = ""
+        limpa5.value = ""
+    }
+
+    // Local pra inserir o elemento na paǵina
+    document.querySelector(clonarEm).appendChild(elementoClone);
 }
